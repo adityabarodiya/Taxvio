4d9ae438d304009e1a143fcd032fa8c66488ed67
adityabarodiya <adi25kht@gmail.com>
2026-05-21 10:44:55 +0530

    gerated Sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,12 +1,45 @@
 /** @type {import('next-sitemap').IConfig} */
+
+const siteUrl = "https://taxvio.in";
+
+const services = [
+  "gst-registration",
+  "fssai-license",
+  "trademark-registration",
+];
+
+const cities = [
+  "delhi",
+  "meerut",
+  "muzaffarnagar",
+];
+
 module.exports = {
-  siteUrl: 'https://taxvio.in',
+  siteUrl,
 
   generateRobotsTxt: true,
 
   sitemapSize: 5000,
 
-  changefreq: 'weekly',
+  additionalPaths: async (config) => {
+    const paths = [];
 
-  priority: 0.7,
-}
+    services.forEach((service) => {
+      paths.push({
+        loc: `/services/${service}`,
+        lastmod: new Date().toISOString(),
+      });
+    });
+
+    cities.forEach((city) => {
+      services.forEach((service) => {
+        paths.push({
+          loc: `/${city}/${service}`,
+          lastmod: new Date().toISOString(),
+        });
+      });
+    });
+
+    return paths;
+  },
+};
